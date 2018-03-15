ec76928ab7866f61175e5f885d21a86957489ed3
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-15 12:18:36 -0400

    WIP

--- Control Server/server/remote_physics_lab/remote_physics_lab/urls.py
+++ Control Server/server/remote_physics_lab/remote_physics_lab/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.urls import path
+
+from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
