92cce0c48ec6cc0f8e7fb9e98e65696ed05e1f27
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-15 11:43:41 -0400

    Fixed it

--- Control Server/remote-physics-lab/src/App.js
+++ Control Server/remote-physics-lab/src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             </div>
         
             {/* YouTube Player for the stream */}
-            <div class="card" style="width: 18rem;">
+            <div class="card mb-3">
                 <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22599%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20599%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1622a31f830%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A30pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1622a31f830%22%3E%3Crect%20width%3D%22599%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22222.765625%22%20y%3D%22103.80000000000001%22%3E599x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"></img>
                 <div class="card-body">
                     <h5 class="card-title">Live Video</h5>
