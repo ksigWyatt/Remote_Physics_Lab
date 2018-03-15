38016ad6321c5696c3de4f6a1b23c93fd286bbc8
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-15 13:28:13 -0400

    Making it look better

--- Control Server/remote-physics-lab/src/App.js
+++ Control Server/remote-physics-lab/src/App.js
@@ -14,48 +14,61 @@ class App extends Component {
         
             {/* YouTube Player for the stream */}
             <div class="card mb-3">
-                <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22599%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20599%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1622a31f830%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A30pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1622a31f830%22%3E%3Crect%20width%3D%22599%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22222.765625%22%20y%3D%22103.80000000000001%22%3E599x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"></img>
+            <iframe  align="center" width="560" height="315" src="https://www.youtube.com/embed/rAlZRFZTwxM?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
                 <div class="card-body">
                     <h5 class="card-title">Live Video</h5>
+                    <br />
                     <button type="button" class="btn btn-primary">Request Access</button>
                     <p class="card-text"><small class="text-muted">X students waiting</small></p>
                 </div>
             </div>
 
-            <div class="btn-group" role="group" aria-label="Basic example">
-                <button type="button" class="btn btn-secondary">Left</button>
-                <button type="button" class="btn btn-secondary">Middle</button>
-                <button type="button" class="btn btn-secondary">Right</button>
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card border-light">
+                    <div class="card-body">
+                        <h5 class="card-title">Deflecting Voltage</h5>
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-secondary">Positive</button>
+                            <button type="button" class="btn btn-secondary">Off</button>
+                            <button type="button" class="btn btn-secondary">Negative</button>
+                        </div>
+                    </div>
+                    </div>
+                </div> 
+                <div class="col-sm-6">
+                    <div class="card border-light">
+                    <div class="card-body">
+                        <h5 class="card-title">Magnetizing Current</h5>
+                        
+                        <div class="slidecontainer">
+                            <input id="curr-slider" type="range" min="0" value="0" max="3" step="0.05" class="slider"></input>
+                            <p id="curr-value"></p>
+                        </div>
+                        <div class="btn-group" role="group" aria-label="Basic example">
+                            <button type="button" class="btn btn-secondary">Clockwise</button>
+                            <button type="button" class="btn btn-secondary">Off</button>
+                            <button type="button" class="btn btn-secondary">Counter-CW</button>
+                        </div>
+                    </div>
+                    </div>
+                </div>
             </div>
 
             <div class="row">
-            <div class="col-sm-6">
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                <div class="col-sm-6">
+                    <div class="card border-light">
+                    <div class="card-body">
+                        <h5 class="card-title">Accelerating Voltage</h5>
+                        {/* <slider></slider> */}
+                        <div class="slidecontainer">
+                        {/* TODO: Using the Library to create the sliders for react */}
+                            <input id="volt-acc-slider" type="range" min="0" max="250" step="1" value="0" class="slider"></input>
+                            <p id="volt-acc-value"></p>
+                        </div>
+                    </div>
+                    </div>
                 </div>
-                </div>
-            </div> 
-            <div class="col-sm-6">
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-            </div>
-            <div class="col-sm-6">
-                <div class="card">
-                <div class="card-body">
-                    <h5 class="card-title">Special title treatment</h5>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-            </div>
             </div>
         </div>
 
