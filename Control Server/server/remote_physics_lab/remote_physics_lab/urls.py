2e9816ba9b392d53019461aa5cd8e94bc5726573
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-15 17:28:38 -0400

    Adding app/ index

--- Control Server/server/remote_physics_lab/remote_physics_lab/urls.py
+++ Control Server/server/remote_physics_lab/remote_physics_lab/urls.py
@@ -15,12 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.urls import path
 
-from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    url(r'^app/', include('RPL.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls')),
 ]
