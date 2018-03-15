5795acf38ec13b93f0df166d29986fd99b3d809f
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-15 11:41:19 -0400

    WIP

--- Control Server/remote-physics-lab/src/App.js
+++ Control Server/remote-physics-lab/src/App.js
@@ -1,92 +1,69 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+
     return (
-      <div className="App">
-        <h2>Remote Physics Lab</h2>
+
+        <div className="App">
+            <div class="jumbotron">
+            <h1 class="display-4">Remote Physics Lab</h1>
+            <p class="lead">Helping students learn Physics from anyhere</p>
+            </div>
         
-        <div class="row">
-            <div class="column left1"></div>
-            <div class="column middle1">
-                <div id="video-feed" class="video">
-                    <iframe src="_" frameborder="2"></iframe>
-                    <h3>Live Video</h3>
+            {/* YouTube Player for the stream */}
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22599%22%20height%3D%22180%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20599%20180%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1622a31f830%20text%20%7B%20fill%3Argba(255%2C255%2C255%2C.75)%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A30pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1622a31f830%22%3E%3Crect%20width%3D%22599%22%20height%3D%22180%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22222.765625%22%20y%3D%22103.80000000000001%22%3E599x180%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" alt="Card image cap"></img>
+                <div class="card-body">
+                    <h5 class="card-title">Live Video</h5>
+                    <button type="button" class="btn btn-primary">Request Access</button>
+                    <p class="card-text"><small class="text-muted">X students waiting</small></p>
                 </div>
             </div>
-            <div class="column right1"></div>
-        </div>
 
-        <br />
-        <br />
-        <br />
-
-        <div id="controls" class="row">
-            <button class="button">Request Access</button>
-            <p id="queue">x students ahead of you</p>
-
-            <br />
-            <br />
-
-            <h3>Controls</h3>
-            <p id="alert-message"></p>
-            <div class="centered">
-                <button class="button">Reset</button>
-                
+            <div class="btn-group" role="group" aria-label="Basic example">
+                <button type="button" class="btn btn-secondary">Left</button>
+                <button type="button" class="btn btn-secondary">Middle</button>
+                <button type="button" class="btn btn-secondary">Right</button>
             </div>
-            
-            <div class="two-row">
-                <div class="two-column">
-                    <h4>Accelerating Voltage</h4>
 
-                    <div class="slidecontainer">
-                        <input id="volt-acc-slider" type="range" min="0" max="250" step="1" value="0" class="slider"></input>
-                        <p id="volt-acc-value"></p>
-                    </div>
-
-                    <br />
-                    <br />
-
-                    <h4>Deflecting Voltage</h4>
-                    <div class="slidecontainer">
-                        <input id="volt-def-slider" type="range" min="50" max="250" step="1" value="0" class="slider"></input>
-                        <p id="volt-def-value"></p>
-                        <p id="volt-selected-value">value of voltage button</p>
-                    </div>
-
-                    <br />
-                    <br />
-
-                    <div class="btn-group">
-                        <button class="button-stack1">Positive</button>
-                        <button class="button-stack1">Off</button>
-                        <button class="button-stack1">Negative</button>
-                    </div>
+            <div class="row">
+            <div class="col-sm-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
                 </div>
-                <div class="two-column">
-                    <h4>Magnetizing Current</h4>
-
-                    <div class="slidecontainer">
-                        <input id="curr-slider" type="range" min="0" value="0" max="3" step="0.05" class="slider"></input>
-                        <p id="curr-value"></p>
-                        <p id="curr-selected-value">value of current button</p>
-                    </div>
-                    <br />
-                    <br />
-                    <h5 style="text-align: right;">Magnetic Arc</h5>
-                    <div class="btn-group">
-                        <button class="button-stack2">Clockwise</button>
-                        <button class="button-stack2">Off</button>
-                        <button class="button-stack2">Counter CW</button>
-                    </div>
                 </div>
+            </div> 
+            <div class="col-sm-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">Special title treatment</h5>
+                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+                </div>
+            </div>
             </div>
         </div>
-      </div>
+
+        
     );
   }
+
+  
 }
 
 export default App;
