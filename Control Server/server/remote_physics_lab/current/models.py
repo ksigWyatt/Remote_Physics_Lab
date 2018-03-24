1fc69799064a98ff4078c2b2ffe443a767c6d4cd
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-24 10:21:31 -0400

    Start understanding what Models do

--- Control Server/server/remote_physics_lab/current/models.py
+++ Control Server/server/remote_physics_lab/current/models.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Current(models.Model):
+    value = models.IntegerField()
+    last_name = models.CharField(max_length=30)
+    updated = models.DateField(null=True)
