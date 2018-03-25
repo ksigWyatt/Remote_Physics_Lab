692f66dfb32f4e2d051e780c3610b2c05cbe505f
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-25 01:55:24 -0400

    add quick install bash scripts to make life a little easier

---
+++ Control Server/server/Dependencies/quick-install-packages.bat
@@ -0,0 +1,4 @@
+REM windows install dependencies with command prompt
+pip install django==1.11
+pip install djangorestframework
+pip install coreapi
---
+++ Control Server/server/Dependencies/quick-install-packages.command
@@ -0,0 +1,3 @@
+pip install django==1.11
+pip install djangorestframework
+pip install coreapi
