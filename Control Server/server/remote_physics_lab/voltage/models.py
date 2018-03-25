1a6d48bec1117320a7d84ec75a89e4e457bc1fa4
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-25 02:27:59 -0400

    Begin models for voltage

--- Control Server/server/remote_physics_lab/voltage/models.py
+++ Control Server/server/remote_physics_lab/voltage/models.py
@@ -1,6 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils import timezone
+import datetime
 
-# Create your models here.
+class Voltage(models.Model):
+    value = models.IntegerField()
+    last_name = models.CharField(max_length=30)
+    updated = models.DateField(null=True)
+
+    # Do we need to keep this?
+    def was_updated_recently(self):
+        return self.updated >= timezone.now() - datetime.timedelta(days=1)
