c5fa87ac7712f33144226686ea8c62cd81e9ada2
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-03-25 01:58:16 -0400

    update pip first before running the scripts

--- Control Server/server/Dependencies/quick-install-packages.bat
+++ Control Server/server/Dependencies/quick-install-packages.bat
@@ -1,4 +1,5 @@
 REM windows install dependencies with command prompt
+python -m pip install --upgrade pip
 pip install django==1.11
 pip install djangorestframework
 pip install coreapi
--- Control Server/server/Dependencies/quick-install-packages.command
+++ Control Server/server/Dependencies/quick-install-packages.command
@@ -1,3 +1,4 @@
+python -m pip install --upgrade pip
 pip install django==1.11
 pip install djangorestframework
 pip install coreapi
