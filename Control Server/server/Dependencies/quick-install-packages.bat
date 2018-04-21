8a2c8e8b1b8c7c34f7b3702099cf3c00b8899f6e
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-04-21 00:12:10 -0400

    Dependency updates

--- Control Server/server/Dependencies/quick-install-packages.bat
+++ Control Server/server/Dependencies/quick-install-packages.bat
@@ -3,5 +3,4 @@ REM windows install dependencies with command prompt
 ECHO Installing Dependencies for you
 python -m pip install --upgrade pip
 pip install django==1.11
-pip install djangorestframework
-pip install coreapi
+pip install django-tastypie
--- Control Server/server/Dependencies/quick-install-packages.command
+++ Control Server/server/Dependencies/quick-install-packages.command
@@ -1,5 +1,4 @@
 echo Installing Dependencies for you
 python -m pip install --upgrade pip
 pip install django==1.11
-pip install djangorestframework
-pip install coreapi
+pip install django-tastypie
