7cc6bc96958012b9b8508a6b4d362f43e37b6046
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-04-26 15:01:59 -0400

    Adding test for GET of vars

--- Control Server/server/remote_physics_lab/RPL/tests/test_vars.py
+++ Control Server/server/remote_physics_lab/RPL/tests/test_vars.py
@@ -1,7 +1,8 @@
 from django.test import TestCase
 from RPL.models import Rpl
+from tastypie.test import ResourceTestCaseMixin
 
-class VarsTestCase(TestCase):
+class VarsTestCase(ResourceTestCaseMixin, TestCase):
     def setUp(self):
         Rpl.objects.create(variables="accelv", values=0, currentvalue=0)
         Rpl.objects.create(variables="current", values=0, currentvalue=1.0)
@@ -17,3 +18,9 @@ class VarsTestCase(TestCase):
         
         r2 = Rpl.objects.get(variables="current")
         self.assertEqual(r2.currentvalue, 1.0)
+
+    def test_get_vars(self):
+        """Testing that the API can return all data from vars via GET"""
+
+        resp = self.client.get('/api/variables/', HTTP_HOST='docs.djangoproject.dev:8000')
+        self.assertEqual(resp.status_code, 200)
