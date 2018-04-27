62b8e65550c73beee2a5b5445fa54f10a11faad8
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-04-26 21:44:51 -0400

    Adding POST, PUT and Delete for queue

--- Control-Server/server/remote_physics_lab/RPL/tests/test_users.py
+++ Control-Server/server/remote_physics_lab/RPL/tests/test_users.py
@@ -1,19 +1,52 @@
 from django.test import TestCase
 from RPL.models import Users
-from tastypie.test import ResourceTestCaseMixin
+from tastypie.test import ResourceTestCaseMixin, TestApiClient
 
 class UserTestCase(TestCase):
     def setUp(self):
         Users.objects.create(ip="127.0.0.1", place=999)
+        Users.objects.create(ip="127.0.0.1", place=998)
         
     def test_user_created(self):
-        """User was successfully created"""
+        """ User was successfully created """
         
         u1 = Users.objects.get(place=999)
         self.assertEqual(u1.ip, "127.0.0.1")
 
     def test_get_queue(self):
-        """Testing that the API can return all data from users via GET"""
+        """ Testing that the API can return all data from users via GET """
 
         resp = self.client.get('/api/queue/', HTTP_HOST='docs.djangoproject.dev:8000')
         self.assertEqual(resp.status_code, 200)
+
+    def test_post_queue(self):
+        """ Performs POST request to queue """
+        client = TestApiClient()
+
+        response = client.post('/api/queue/', data={
+            'ip': '127.0.0.1',
+            'place': 997
+        })
+
+        # 201 == Created
+        self.assertEqual(response.status_code, 201)
+
+    def test_put_queue(self):
+        """ Performs PUT request for queue id=1"""
+        client = TestApiClient()
+
+        response = client.put('/api/queue/2/', data={
+            'place': 990
+        })
+
+        # 204 == Action Performed -> No Data
+        self.assertEqual(response.status_code, 204)
+
+    def test_delete_queue_item(self):
+        """ Performs DELETE request to queue id 1"""
+        client = TestApiClient()
+
+        response = client.delete('/api/queue/2/',data={'format': 'json'})
+
+        # 204 == Action Performed -> No Data
+        self.assertEqual(response.status_code, 204)
