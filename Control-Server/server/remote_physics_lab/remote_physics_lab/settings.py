aefda4d9a54b93ef6522c2383536259d400faf5d
Wyatt Baggett <wyattbaggett@yahoo.com>
2018-04-26 22:46:30 -0400

    adding code coverage if needed

--- Control-Server/server/remote_physics_lab/remote_physics_lab/settings.py
+++ Control-Server/server/remote_physics_lab/remote_physics_lab/settings.py
@@ -39,7 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'RPL',
     'corsheaders',
+    # 'django_nose', # enable this for code coverage
 ]
+
+# # Use nose to run all tests
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+# # Tell nose to measure coverage on the 'foo' and 'bar' apps
+# NOSE_ARGS = [
+#     '--with-coverage',
+#     '--cover-package=RPL'
+# ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
